94d579f5fdede2a56d441a33569a5ad07df6f5bf
Daniel Johns <civatrix@gmail.com>
2025-12-01 21:32:25 -0800

    Day 2 Part 1

--- Day2/Day2.swift
+++ Day2/Day2.swift
@@ -9,6 +9,22 @@ import Foundation
 
 final class Day2: Day {
     func run(input: String) -> String {
-        return ""
+        var sum = 0
+        var ranges = [ClosedRange<Int>]()
+        for pair in input.split(separator: ",") {
+            let pair = pair.bifurcate(on: "-")
+            
+            ranges.append(Int(pair.0)!...Int(pair.1)!)
+        }
+        
+        for range in ranges {
+            for candidate in range.map({ $0.description }) where candidate.count.isMultiple(of: 2) {
+                if candidate.prefix(candidate.count / 2) == candidate.suffix(candidate.count / 2) {
+                    sum += Int(candidate)!
+                }
+            }
+        }
+        
+        return sum.description
     }
 }
--- Day2/Day2Tests.swift
+++ Day2/Day2Tests.swift
@@ -13,7 +13,8 @@ final class Day2Tests: XCTestCase {
     func testDay() throws {
         let input =
 """
+11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124
 """
-        XCTAssertEqual(day.run(input: input), "")
+        XCTAssertEqual(day.run(input: input), "1227775554")
     }
 }
