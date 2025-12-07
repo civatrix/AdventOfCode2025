8534f18b440ee7ae2f89d5d12671fe927273c84e
Daniel Johns <civatrix@gmail.com>
2025-12-06 21:37:28 -0800

    Day 7 Part 1

--- Day7/Day7.swift
+++ Day7/Day7.swift
@@ -9,6 +9,20 @@ import Foundation
 
 final class Day7: Day {
     func run(input: String) -> String {
-        return ""
+        let splitters = input.parseGrid(target: "^").sorted()
+        let start = input.firstIndex(of: "S")!.utf16Offset(in: input)
+        var open: Set<Point> = [[start, 0]]
+        var splits = Set<Point>()
+        
+        while let beam = open.popFirst() {
+            if let splitter = splitters.first(where: { $0.x == beam.x && $0.y > beam.y }) {
+                if splits.insert(splitter).inserted {
+                    open.insert(splitter + .left)
+                    open.insert(splitter + .right)
+                }
+            }
+        }
+        
+        return splits.count.description
     }
 }
