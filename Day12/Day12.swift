735a4d362303c83feb6e6c8a86c621d5e9d40374
Daniel Johns <civatrix@gmail.com>
2026-01-03 18:00:39 -0800

    Day 12 Part 2

--- Day12/Day12.swift
+++ Day12/Day12.swift
@@ -9,6 +9,11 @@ import Foundation
 
 final class Day12: Day {
     func run(input: String) -> String {
-        return ""
+        return input.lines
+            .filter { $0.contains("x") }
+            .map { $0.allDigits }
+            .filter { $0[0] * $0[1] >= $0.dropFirst(2).sum * 9 }
+            .count
+            .description
     }
 }
--- Day12/Day12Tests.swift
+++ Day12/Day12Tests.swift
@@ -13,7 +13,40 @@ final class Day12Tests: XCTestCase {
     func testDay() throws {
         let input =
 """
+0:
+###
+##.
+##.
+
+1:
+###
+##.
+.##
+
+2:
+.##
+###
+##.
+
+3:
+##.
+###
+##.
+
+4:
+###
+#..
+###
+
+5:
+###
+.#.
+###
+
+4x4: 0 0 0 0 2 0
+12x5: 1 0 1 0 2 2
+12x5: 1 0 1 0 3 2
 """
-        XCTAssertEqual(day.run(input: input), "")
+        XCTAssertEqual(day.run(input: input), "2")
     }
 }
